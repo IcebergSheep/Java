4b67ffeadf21b340810e96b97ab8a5c6e55773a0
IcebergSheep <101859625+IcebergSheep@users.noreply.github.com>
2022-03-18 16:35:21 +0900

    Update 2588.java

--- 2588.java
+++ 2588.java
@@ -1,1 +1,19 @@
+package lec01;
+
+import java.util.Scanner;
+
+public class b2588 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a*(b%10));
+		System.out.println(a*(b%100/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+	}
+
+}
 

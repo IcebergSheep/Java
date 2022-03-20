23c7df4eb388950e5ac54613856320ee61e80d68
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-03-20 20:41:05 +0900

    Update and rename 2884 to 2884.java

---
+++ 2884.java
@@ -0,0 +1,34 @@
+package Caerang.src.lec01;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class b2884 {
+    public static void main(String[] args){
+        System.out.println("Scan H and M");
+        Scanner input = new Scanner(System.in);
+        int h = input.nextInt();
+        int m = input.nextInt();
+
+        if (m >= 45) {
+            m -= 45;
+        }
+        if(m == 0){
+            m += 15;
+            h -= 1;
+            if(h<=1){
+                h = 12;
+            }
+        }
+        else {
+            m += 15;
+            h -= 1;
+            if(h<=1){
+                h = 12;
+            }
+        }
+        System.out.println(h+" "+m);
+    }
+}
+

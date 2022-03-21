dfab17df15ae230c76b6d1fbc9b6fb4e9881d522
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-03-21 16:55:34 +0900

    Update 2530.java

--- 2530.java
+++ 2530.java
@@ -1,1 +1,14 @@
+package Caerang.src.lec01;
+
+import java.util.Scanner;
+
+public class b2530 {
+    public static void main(String[] args){
+        System.out.println("시 분 초 단위로 입력하세요>");
+        Scanner input = new Scanner(System.in);
+        int h = input.nextInt();
+        int m = input.nextInt();
+        int s = input.nextInt();
+    }
+}
 

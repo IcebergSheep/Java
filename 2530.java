bdc113886097e4c79f4943a5e0c24590dadc0da4
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-03-21 19:47:58 +0900

    Update 2530.java

--- 2530.java
+++ 2530.java
@@ -6,11 +6,16 @@ public class b2530 {
     public static void main(String[] args){
         System.out.println("시 분 초 단위로 입력하세요>");
         Scanner input = new Scanner(System.in);
-        int h = input.nextInt();
-        int m = input.nextInt();
-        int s = input.nextInt();
+        int h = input.nextInt(); // hour
+        int m = input.nextInt(); // minute
+        int s = input.nextInt(); // second
         
         int a = input.nextInt();
+        a += s;
+        
+        h =% 24;
+        m =% 60;
+        s %= 60;
         
     }
 }
